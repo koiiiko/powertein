c42dfb612cf2844105ff91c7b14ae20417b42e87
ribkaadevina <ribkaadevina@gmail.com>
2025-04-29 17:50:09 +0700

    adding sidebarlayout and css config

---
+++ fe-service/src/components/sidebarlayout.jsx
@@ -0,0 +1,103 @@
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import {
+  Calculator,
+  NotebookPen,
+  ShoppingCart,
+  Newspaper,
+  Bot,
+  ChevronRight,
+} from "lucide-react";
+
+const SidebarLayout = ({ children }) => {
+  const [activeLink, setActiveLink] = useState("");
+  const handleLinkClick = (linkName) => {
+    setActiveLink(linkName);
+  };
+  return (
+    <div className="grid grid-cols-12 h-screen">
+      {/* Sidebar */}
+      <aside className="col-span-2 flex flex-col overflow-hidden text-center bg-[#F1F4F9] ">
+        <h1 className="font-semibold text-gray-800 my-5 md:text-3xl ">
+          PowerTein
+        </h1>
+        {/* Sidebar Menu */}
+        <nav className="flex flex-col gap-3 text-left mx-3">
+          <Link
+            to="/"
+            className={`${
+              activeLink === "protein-calculator"
+                ? "text-[#45556C] bg-white font-bold"
+                : "text-[#91A1BA]"
+            } hover:text-gray-900 p-2 flex md:text-[14px] rounded-lg`}
+            onClick={() => handleLinkClick("protein-calculator")}
+          >
+            <Calculator className="me-3" />
+            Protein Calculator
+          </Link>
+          <Link
+            to="/"
+            className={`${
+              activeLink === "protein-tracker"
+                ? "text-[#45556C] bg-white font-bold"
+                : "text-[#91A1BA]"
+            } hover:text-gray-900 p-2 flex md:text-[14px] rounded-lg`}
+            onClick={() => handleLinkClick("protein-tracker")}
+          >
+            <NotebookPen className="me-3" />
+            Protein Tracker
+          </Link>
+          <Link
+            to="/"
+            className={`${
+              activeLink === "proteimart"
+                ? "text-[#45556C] bg-white font-bold"
+                : "text-[#91A1BA]"
+            } hover:text-gray-900 p-2 flex md:text-[14px] rounded-lg`}
+            onClick={() => handleLinkClick("proteimart")}
+          >
+            <ShoppingCart className="me-3" />
+            ProteinMart
+          </Link>
+          <Link
+            to="/"
+            className={`${
+              activeLink === "proteinpedia"
+                ? "text-[#45556C] bg-white font-bold"
+                : "text-[#91A1BA]"
+            } hover:text-gray-900 p-2 flex md:text-[14px] rounded-lg`}
+            onClick={() => handleLinkClick("proteinpedia")}
+          >
+            <Newspaper className="me-3" />
+            ProteinPedia
+          </Link>
+          <Link
+            to="/"
+            className={`${
+              activeLink === "proteinbot"
+                ? "text-[#45556C] bg-white font-bold"
+                : "text-[#91A1BA]"
+            } hover:text-gray-900 p-2 flex md:text-[14px] rounded-lg`}
+            onClick={() => handleLinkClick("proteinbot")}
+          >
+            <Bot className="me-3" />
+            ProteinBot
+          </Link>
+        </nav>
+        {/* Sidebar User Info */}
+        <div className="flex px-2 items-center mt-auto my-8 mx-3">
+          <div className="flex flex-col text-left text-gray-500">
+            <p className="font-semibold text-gray-700 md:text-[14px]">Mike Den</p>
+            <p className="md:text-xs">mike.den@example.com</p>
+          </div>
+          <ChevronRight size={24} className="ml-3 flex text-gray-500" />
+        </div>
+      </aside>
+
+      {/* Content */}
+      <main className="col-span-10 overflow-y-auto p-6">{children}</main>
+    </div>
+  );
+};
+
+export default SidebarLayout;
--- fe-service/src/pages/Home.jsx
+++ fe-service/src/pages/Home.jsx
@@ -3,17 +3,25 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { Button } from "@heroui/react";
 import { User } from "lucide-react";
+import SidebarLayout from "../components/sidebarlayout";
 
 const Homepage = () => {
   return (
-    <div className="container mx-auto p-4">
-      <h1 className="text-3xl font-bold mb-4">Welcome to Powertein</h1>
-      <div className="flex gap-4">
-        <Button color="primary" as={Link} to="/auth">
-          <User size={22}/> Auth
-        </Button>
+    <SidebarLayout>
+      <div className="">
+        <h1 className="">Welcome to Powertein</h1>
+        <div className="">
+          <Button color="primary" as={Link} to="/auth">
+            <User size={22} /> Auth
+          </Button>
+          <hr />
+          <Button color="primary" as={Link} to="/auth" className="h-[1000px] ">
+            <User size={22} /> Auth
+          </Button>
+          <hr />
+        </div>
       </div>
-    </div>
+    </SidebarLayout>
   );
 };
 
