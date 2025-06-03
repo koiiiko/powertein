07964deb6946da6d1c5483840cab69ff57d3d741
koiiiko <mrifqihdyt12@gmail.com>
2025-06-03 08:21:29 +0700

    fix article schema

--- be-service/models.js
+++ be-service/models.js
@@ -8,7 +8,7 @@ con.connect(function (err) {
   console.log("Connected!");
   var usersSchema = "CREATE TABLE users (id INT PRIMARY KEY AUTO_INCREMENT, username VARCHAR(50) UNIQUE NOT NULL, email VARCHAR(100) UNIQUE NOT NULL, password VARCHAR(255) NOT NULL, created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, gender ENUM('Laki-laki', 'Perempuan'), age INT, height INT, weight INT, activityLevel ENUM('aktivitasMinimal', 'aktivitasRendah', 'aktivitasSedang', 'aktivitasTinggi', 'aktivitasSangatTinggi'), protein INT, updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP)";
 
-  var articleSchema = "CREATE TABLE article (id INT PRIMARY KEY AUTO_INCREMENT, title VARCHAR(255) NOT NULL, user_id INT NOT NULL, username VARCHAR(50) NOT NULL, image BLOB, content TEXT, created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP)";
+  var articleSchema = "CREATE TABLE article (id INT PRIMARY KEY AUTO_INCREMENT, title VARCHAR(255) NOT NULL, user_id INT NOT NULL, username VARCHAR(50) NOT NULL, image BLOB, content TEXT, created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, FOREIGN KEY (user_id) REFERENCES users(id))";
   //add your create table schema here
 
   con.query(usersSchema, function (err, result) {
