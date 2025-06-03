b3d03f11158369a6bf205125c0e8c7caa1b17083
ribkaadevina <ribkaadevina@gmail.com>
2025-06-03 12:00:31 +0700

    add protected routes

--- fe-service/src/router.jsx
+++ fe-service/src/router.jsx
@@ -1,48 +1,41 @@
 import { createBrowserRouter } from "react-router-dom";
-import HomePage from "./pages/Home";
-import LoginPage from "./features/rifqi-auth/pages/Login";
+import ProtectedRoute from "./components/protectedRoute";
 import SignupPage from "./features/rifqi-auth/pages/SignUp";
+import LoginPage from "./features/rifqi-auth/pages/Login";
+import HomePage from "./pages/Home";
 import Forum from "./features/ribka-forum/pages/Forum";
 import ArticleDetail from "./features/ribka-forum/pages/ArticleDetail";
 // import ProteinCalculatorPage from './features/radit-calculator/pages/ProteinCalculator';
 
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: <HomePage />,
-    children: [
-      {
-        index: true,
-        element: <HomePage />,
-      },
-      {
-        path: "/forum",
-        element: <Forum />,
-      },
-      {
-        path: "/forum/:id",
-        element: <ArticleDetail />,
-      },
-      //   {
-      //     path: 'protein-calculator',
-      //     element: <ProteinCalculatorPage />,
-      //   },
-      // Add more protected routes here if needed
-    ],
+    path: "/signup",
+    element: <SignupPage />,
   },
   {
     path: "/login",
     element: <LoginPage />,
   },
   {
-    path: "/signup",
-    element: <SignupPage />,
+    path: "/",
+    element: (
+      <ProtectedRoute>
+        <HomePage />
+      </ProtectedRoute>
+    ),
   },
-  // Legacy auth route (redirect to login)
   {
-    path: "/auth",
-    element: <LoginPage />,
+    path: "/forum",
+    element: <Forum />,
   },
+  {
+    path: "/forum/:id",
+    element: <ArticleDetail />,
+  },
+  // {
+  //   path: "/calculator",
+  //   element: <ProteinCalculatorPage />,
+  // },
 ]);
 
 export default router;
