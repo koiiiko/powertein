bdda2f5cd7ecb451319a8ca2a5ece5e70670efb0
koiiiko <mrifqihdyt12@gmail.com>
2025-06-20 17:29:40 +0700

    fix vite config command

--- fe-service/vite.config.js
+++ fe-service/vite.config.js
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 import vercel from 'vite-plugin-vercel';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), vercel()],
   build: {
